1eef24b6b1ee9c7ab2ed85ecde92b6345641fdb8
CellarstoneDevTeam <53124582+CellarstoneDevTeam@users.noreply.github.com>
2019-08-14 13:30:18 +0530

    Update CopyScriptsFromJenkinWorkSpaceTOBuildScriptFolder.groovy

--- CopyScriptsFromJenkinWorkSpaceTOBuildScriptFolder.groovy
+++ CopyScriptsFromJenkinWorkSpaceTOBuildScriptFolder.groovy
@@ -14,7 +14,8 @@ node
     echo "if  exist " +  QCVSSWorkFolder.replace("\\\\", "\\") + "\\BuildScript\\DBBackup\\qcomission.bak" + " del " +  QCVSSWorkFolder.replace("\\\\", "\\") + "\\BuildScript\\DBBackup\\qcommission.bak"      
     bat "copy \"" + QCommDBPath + '\\Qcommission\\*.*\" ' +  QCVSSWorkFolder.replace("\\\\", "\\") + "\\BuildScript"
     /*bat "copy \"" + QCommDBPath + "\\Qcommission\\*.*\\"  +  QCVSSWorkFolder.replace("\\\\", "\\") + "\\BuildScript\\DBBackup\" */
-    bat " ren " +  QCVSSWorkFolder.replace("\\\\", "\\") + "\\BuildScript\\DBBackup\qcommission\\*.* "  +  QCVSSWorkFolder.replace("\\\\", "\\") + "\\BuildScript\\DBBackup\\qcommission.bak"  
+    bat "ren \"" + QCVSSWorkFolder.replace("\\\\", "\\") + '\\BuildScript\\DBBackup\qcommission\\*.* " ' +  QCVSSWorkFolder.replace("\\\\", "\\") + "\\BuildScript\\DBBackup\\qcommission.bak"  
+    echno " ren \"" +  QCVSSWorkFolder.replace("\\\\", "\\") + "\\BuildScript\\DBBackup\qcommission\\*.* "  +  QCVSSWorkFolder.replace("\\\\", "\\") + "\\BuildScript\\DBBackup\\qcommission.bak"  
      bat "copy \"" + QCommDBPath + '\\Sample_Service*.*\" ' +  QCVSSWorkFolder.replace("\\\\", "\\") + "\\BuildScript\\DBBackup\\Sample_Service.bak"      */
     
      
