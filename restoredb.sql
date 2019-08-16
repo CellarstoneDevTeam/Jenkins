d60e5ab3bdb84976b6c9dd3b569ac604df0e94db
CellarstoneDevTeam <53124582+CellarstoneDevTeam@users.noreply.github.com>
2019-08-16 20:37:34 +0530

    Update restoredb.sql

--- restoredb.sql
+++ restoredb.sql
@@ -1,6 +1,6 @@
 Declare @SQL_SCRIPT varchar(max)
 declare @sqlstr varchar(max)
-
+declare @BackupFileName varchar(max)
 DECLARE @file_name_d nvarchar(200) = CONVERT(sysname, SERVERPROPERTY('InstanceDefaultDataPath'))
 DECLARE @file_name_l nvarchar(200) = CONVERT(sysname, SERVERPROPERTY('InstanceDefaultLogPath'))
 
@@ -34,7 +34,7 @@ BEGIN
         Print 'Log file location = '+@file_name_l+@SQLDB+'.ldf';
    
 END
-set @sqlstr = 'ALTER DATABASE +' '+@SQLDB+' '
+set @sqlstr = 'ALTER DATABASE '+ @SQLDB+' '
 SET @sqlstr =  @sqlstr +'SET SINGLE_USER WITH '
 SET @sqlstr =  @sqlstr +'ROLLBACK IMMEDIATE'
 EXEC (@sqlstr);
