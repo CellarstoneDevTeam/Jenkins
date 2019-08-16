ae7d996d182d07d86e74ffccdb84e4a73dcd9a51
CellarstoneDevTeam <53124582+CellarstoneDevTeam@users.noreply.github.com>
2019-08-16 19:03:56 +0530

    Update restoredb.sql

--- restoredb.sql
+++ restoredb.sql
@@ -1,29 +1,29 @@
 Declare @SQL_SCRIPT varchar(max)
-IF  NOT EXISTS (SELECT * FROM sys.databases WHERE name = N'QCBuild')
+IF  NOT EXISTS (SELECT * FROM sys.databases WHERE name = N'QCommission')
 BEGIN
     
-   CREATE DATABASE QCBuild
+   CREATE DATABASE QCommission
    ON
-   ( NAME = QCBuildMDF,  
-       FILENAME = 'D:\QCBuildMDFFile.mdf',
+   ( NAME = QCommissionMDF,  
+       FILENAME = 'D:\QCommissionMDFFile.mdf',
        SIZE = 10,
        MAXSIZE = 50,
        FILEGROWTH = 5 )  
    LOG ON
-   ( NAME = QCBuildLDF,  
-       FILENAME = 'D:\QCBuildLDFFile.ldf',
+   ( NAME = QCommissionLDF,  
+       FILENAME = 'D:\QCommissionLDFFile.ldf',
        SIZE = 5,
        MAXSIZE = 25,
        FILEGROWTH = 5 )
    
 END
-ALTER DATABASE QCBuild
+ALTER DATABASE QCommission
 SET SINGLE_USER WITH
 ROLLBACK IMMEDIATE
 --$(FTPPath)
 ----Restore Database
 --RESTORE DATABASE QCBuild FROM DISK = @BackupFile WITH replace 
-SET @SQL_SCRIPT = 'RESTORE DATABASE QCBuild FROM DISK = '''+ @FTPPath + '\Qcommission.bak''' + ' WITH REPLACE'
+SET @SQL_SCRIPT = 'RESTORE DATABASE QCommission FROM DISK = '''+ @FTPPath + '\Qcommission.bak''' + ' WITH REPLACE'
 --SET @SQL_SCRIPT = 'RESTORE DATABASE QCBuild FROM DISK = ''' + @FTPPath + '\Qcommission.bak'' WITH replace;'
 --SET @SQL_SCRIPT = 'RESTORE DATABASE QCBuild FROM DISK = ''' + ''' WITH replace;'
 --SET @SQL_SCRIPT = 'RESTORE DATABASE QCBuild FROM DISK = ''' + QUOTENAME(@FTPPath + '\Qcommission.bak') +  'WITH replace;'
@@ -37,6 +37,6 @@ insert into buildlog (logtext) values (@SQL_SCRIPT)*/
 mode.
 If error occurs please execute following command it will convert
 database in multi user.*/
-ALTER DATABASE QCBuild SET MULTI_USER
+ALTER DATABASE QCommission SET MULTI_USER
 GO
 
