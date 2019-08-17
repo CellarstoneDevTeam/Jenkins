94afefd638563a36dd997ee23767cdc5bd18fbd5
CellarstoneDevTeam <53124582+CellarstoneDevTeam@users.noreply.github.com>
2019-08-17 10:30:00 +0530

    Update restoredb.sql

--- restoredb.sql
+++ restoredb.sql
@@ -11,7 +11,7 @@ set @SQLDB = '''' + @SQLDB + ''''
 --IF  NOT EXISTS (SELECT '1' FROM sys.databases WHERE name = @SQLDB)
 --BEGIN
 
-        SET @sqlstr =  'IF  NOT EXISTS (SELECT * FROM sys.databases WHERE name =''' +@SQLDB+ '''
+        SET @sqlstr =  'IF  NOT EXISTS (SELECT * FROM sys.databases WHERE name =''' +@SQLDB+ ''''
         SET @sqlstr =  @sqlstr + ' CREATE DATABASE'+' '+@SQLDB+' '
         SET @sqlstr =  @sqlstr +'ON'
         SET @sqlstr =  @sqlstr +'('    
