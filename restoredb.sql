951673557d0102debbfe87ad28eaf3b26d0ecd4d
CellarstoneDevTeam <53124582+CellarstoneDevTeam@users.noreply.github.com>
2019-08-17 21:04:15 +0530

    Update restoredb.sql

--- restoredb.sql
+++ restoredb.sql
@@ -3,11 +3,11 @@ declare @sqlstr varchar(max)
 declare @BackupFileName varchar(max)
 DECLARE @file_name_d nvarchar(200) = CONVERT(sysname, SERVERPROPERTY('InstanceDefaultDataPath'))
 DECLARE @file_name_l nvarchar(200) = CONVERT(sysname, SERVERPROPERTY('InstanceDefaultLogPath'))
-set @BackupFileName = '''@SQLDB' + '.bak'''
+set @BackupFileName = ''@SQLDB' + '.bak'''
 
                 drop database @SQLDB;                                                   
     CREATE DATABASE  @SQLDB;
-SET @SQL_SCRIPT = 'RESTORE DATABASE ' + '@SQLDB' + ' FROM DISK = '''+ @FTPPath + '\\' + @BackupFileName + ''' WITH REPLACE'
+SET @SQL_SCRIPT = 'RESTORE DATABASE ' + '@SQLDB' + ' FROM DISK = '''+ @FTPPath + '\' + @BackupFileName + ''' WITH REPLACE'
                                                                      
                                     use qcbuild
 delete from buildlog
