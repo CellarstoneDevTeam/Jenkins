8173d1103e3883e6d5ad1c23621f46b40302bba8
CellarstoneDevTeam <53124582+CellarstoneDevTeam@users.noreply.github.com>
2019-08-17 20:45:43 +0530

    Update restoredb.sql

--- restoredb.sql
+++ restoredb.sql
@@ -4,43 +4,16 @@ declare @BackupFileName varchar(max)
 DECLARE @file_name_d nvarchar(200) = CONVERT(sysname, SERVERPROPERTY('InstanceDefaultDataPath'))
 DECLARE @file_name_l nvarchar(200) = CONVERT(sysname, SERVERPROPERTY('InstanceDefaultLogPath'))
 
-/*set @BackupFileName =  concat('''',@FTPPath,'\\''',@SQLDB,'''.bak','''')
 
-IF  NOT EXISTS (SELECT * FROM sys.databases WHERE name = '@SQLDB' )*/
---BEGIN
   drop database @sqldb                                                                   
     CREATE DATABASE  @SQLDB
---END
---RESTORE DATABASE QCBuild FROM DISK = '@FTPPath\@SQLDB.bak' WITH replace 
-                                                                     /*set @sqlstr = 'ALTER DATABASE '+ @SQLDB+' '
-SET @sqlstr =  @sqlstr +'SET SINGLE_USER WITH '
-SET @sqlstr =  @sqlstr +'ROLLBACK IMMEDIATE'
---EXEC (@sqlstr);
-Print (@sqlstr);                                                                     
---$(FTPPath)
-----Restore Database
---RESTORE DATABASE QCBuild FROM DISK = @BackupFile WITH replace */
 SET @SQL_SCRIPT = 'RESTORE DATABASE ' + @SQLDB + ' FROM DISK = '''+ @FTPPath + '\\' + @BackupFileName + ''' WITH REPLACE'
                                                                      
---execute (@sql_script)
-/*--SET @SQL_SCRIPT = 'RESTORE DATABASE QCBuild FROM DISK = ''' + @FTPPath + '\Qcommission.bak'' WITH replace;'
---SET @SQL_SCRIPT = 'RESTORE DATABASE QCBuild FROM DISK = ''' + ''' WITH replace;'
---SET @SQL_SCRIPT = 'RESTORE DATABASE QCBuild FROM DISK = ''' + QUOTENAME(@FTPPath + '\Qcommission.bak') +  'WITH replace;'
---EXECUTE (@SQL_SCRIPT)
-print (@SQL_SCRIPT)*/
-
                                     use qcbuild
 delete from buildlog
 insert into buildlog (logtext) values (@SQL_SCRIPT)
 
  
-/*If there is no error in statement before database will be in multiuser
-mode.
-If error occurs please execute following command it will convert
-database in multi user.*/
---set @sqlstr = 'ALTER DATABASE ' + @SQLDB  + ' SET MULTI_USER'
---EXEC (@sqlstr);                                                                   
-print (@sqlstr); */                                                                   
 GO
 
 
