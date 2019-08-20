f2c15255a4de7ac4910860700ac98b9e539138ab
CellarstoneDevTeam <53124582+CellarstoneDevTeam@users.noreply.github.com>
2019-08-20 15:04:35 +0530

    Update ValidationQuery.sql

--- ValidationQuery.sql
+++ ValidationQuery.sql
@@ -1,13 +1,3 @@
-
-set nocount on
-Select 'Duplicate Columns'
-set nocount off
-SELECT 
-Case When Len(page_table_name) <= 30 
-THEN  page_table_name+Stuff(SPACE(30 -Len(page_table_name)),1, 30 -Len(page_table_name),Replicate(' ',30 -Len(page_table_name))) 
-ELSE page_table_name END + ' - '+  attrib_ID  FROM sy_obj_att INNER JOIN SY_PG ON SY_PG.object_id = sy_obj_att.object_id 
-where page_table_name is not null 
-GROUP BY page_table_name, attrib_ID HAVING COUNT(*)>1 ORDER BY page_table_name, attrib_ID 
 Select 'Missing Dictionary Columns'
 set nocount off
 select 
@@ -20,3 +10,10 @@ INNER JOIN sy_obj_att att ON obj.af_row_id = att.object_id
 where object_table=isc.TABLE_NAME and attrib_id=isc.COLUMN_NAME ) 
 and  table_name in  (select Table_name from INFORMATION_SCHEMA.tables ist where  exists (select * from sy_obj where object_table=ist.TABLE_NAME))
 order by table_name,column_name
+
+SELECT 
+Case When Len(page_table_name) <= 30 
+THEN  page_table_name+Stuff(SPACE(30 -Len(page_table_name)),1, 30 -Len(page_table_name),Replicate(' ',30 -Len(page_table_name))) 
+ELSE page_table_name END + ' - '+  attrib_ID  FROM sy_obj_att INNER JOIN SY_PG ON SY_PG.object_id = sy_obj_att.object_id 
+where page_table_name is not null 
+GROUP BY page_table_name, attrib_ID HAVING COUNT(*)>1 ORDER BY page_table_name, attrib_ID 
