121d0dff3fae0d791d7a2425c554cb20a853d4cd
CellarstoneDevTeam <53124582+CellarstoneDevTeam@users.noreply.github.com>
2019-10-28 16:07:29 +0530

    Update restoredb.sql

--- restoredb.sql
+++ restoredb.sql
@@ -1,7 +1,7 @@
 Declare @SQL_SCRIPT varchar(max)
 declare @sqlstr varchar(max)
 declare @BackupFileName varchar(max)
-DECLARE @SQLDBNAME VARCHAR(MAX)
+declare @SQLDBNAME varchar(max)
 DECLARE @file_name_d nvarchar(200) = CONVERT(sysname, SERVERPROPERTY('InstanceDefaultDataPath'))
 DECLARE @file_name_l nvarchar(200) = CONVERT(sysname, SERVERPROPERTY('InstanceDefaultLogPath'))
 set @BackupFileName = '@SQLDB' + '.bak'''
@@ -10,12 +10,12 @@ set @SQLDBNAME = '@SQLDB'
 IF (EXISTS (SELECT name 
 FROM master.dbo.sysdatabases 
 WHERE ('[' + name + ']' = @SQLDBNAME
-OR name = '@SQLDBNAME')))
+OR name = @SQLDBNAME)))
      
-drop database N'@SQLDB' 
+drop database @SQLDBNAME
 
                                                      
-CREATE DATABASE  N'@SQLDB'
+CREATE DATABASE  @SQLDBNAME
                                                                   
 /*
 
