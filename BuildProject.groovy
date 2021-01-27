f070e1bf15daeabacbe0150e42070d110806b6d0
CellarstoneDevTeam <53124582+CellarstoneDevTeam@users.noreply.github.com>
2021-01-27 16:53:14 +0530

    Update BuildProject.groovy

--- BuildProject.groovy
+++ BuildProject.groovy
@@ -178,8 +178,9 @@ stage(QCVSSProjectFolder + ' Get Source from VSS')
             //println "copy " + QCVSSWorkFolder.replace("\\\\", "\\") + "\\References " + dllpath +  "\\bin"
 	    println " If Exist " + QCVSSWorkFolder.replace("\\\\", "\\") + "\\References copy " + QCVSSWorkFolder.replace("\\\\", "\\") + "\\References " + dllpath +  "\\bin"	
 	    println "solution" + "$it"
-            bat " If Exist " + QCVSSWorkFolder.replace("\\\\", "\\") + "\\References copy " + QCVSSWorkFolder.replace("\\\\", "\\") + "\\References " + dllpath +  "\\bin"
-	    println "before compile"
+                println "before bat"
+		bat " If Exist " + QCVSSWorkFolder.replace("\\\\", "\\") + "\\References copy " + QCVSSWorkFolder.replace("\\\\", "\\") + "\\References " + dllpath +  "\\bin"
+	 println "after bat"
             compilesolution("$it")
             //Get the output assembly Name it can be exe or dll
             def assembly = getassemblyname("$it")
