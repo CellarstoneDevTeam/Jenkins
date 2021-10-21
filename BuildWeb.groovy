ab7d67dd9723ccc9754b3a32cd5b4958f4399369
CellarstoneDevTeam <53124582+CellarstoneDevTeam@users.noreply.github.com>
2021-10-21 19:32:37 +0530

    Update BuildWeb.groovy

--- BuildWeb.groovy
+++ BuildWeb.groovy
@@ -213,7 +213,7 @@ stage('Get Code from VSS')
 	    
 	 git credentialsId: 'qc-robot',url: 'http://code.cellarstone.com:3000/QCommission/QCommissionPortal.git'
 
-          bat "copy \"" + "$WORKSPACE" + '\\\BuildWeb\\*.*\" ' +  QCVSSWorkFolder.replace("\\\\", "\\") + "\\QCommissionPortal"
+          bat "copy \"" + "$WORKSPACE" + '\\BuildWeb\\*.*\" ' +  QCVSSWorkFolder.replace("\\\\", "\\") + "\\QCommissionPortal"
      } 
 	
      stage('Compile Project and Copy DLLS')
