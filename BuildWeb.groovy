1ef4985886fcb6c784ea3abc3206375c1c3f835a
CellarstoneDevTeam <53124582+CellarstoneDevTeam@users.noreply.github.com>
2021-10-22 11:52:27 +0530

    Update BuildWeb.groovy

--- BuildWeb.groovy
+++ BuildWeb.groovy
@@ -223,7 +223,7 @@ stage('Get Code from VSS')
 	    else
 	    {
 	     
-             bat "xcopy \"" + "$WORKSPACE" + '\\' + QCVSSProjectFolder + ' ' +  QCVSSWorkFolder.replace("\\\\", "\\") + "\\QCommissionPortal //E//H//I"
+             print "xcopy \"" + "$WORKSPACE" + '\\' + QCVSSProjectFolder + ' ' +  QCVSSWorkFolder.replace("\\\\", "\\") + "\\QCommissionPortal //E//H//I" 
 	  
 	  
 	    }
